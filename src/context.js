2aa1cbf6da60cd89a85010f466aba1a7741f1721
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-07-09 16:43:11 +0200

    adding provider and reducer

--- src/context.js
+++ src/context.js
@@ -3,58 +3,55 @@ import React, { Component } from 'react'
 // 1st, make a new context
 export const MyContext = React.createContext();
 
-/*
-* REDUCER function with state and action as arguments
-* SWITCH statement with CASES for change to the state
-* And RETURNS the UPDATED state
-*
-* Add a DISPATCH property to the STATE with  
-* FUNCTION as a property 
-* that SETsSTATE by Calling the REDUCER
-*
-* RETURN a <Provider> with a VALUE this.state
-*/
 
-// 2nd, create a provider component
-export class MyProvider extends Component {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT_X":
+      return {
+        ...state,
+        x: state.x + 1
+      };
+    case "DECREMENT_Y":
+      return {
+        ...state,
+        y: state.x - 1
+      };
+    case "MULTIPLY_Z":
+      return {
+        ...state,
+        z: this.state.z * this.state.y
+      };
+    case "DIVIDE_Z":
+      return {
+        ...state,
+        z: this.state.z / this.state.x
+      };
+    default:
+      return state;
 
-  state = {
-    x: 0,
-    y: 100,
-    z: 50
-  };
-  // we return a context provider
-
-  multiplyZ = () => {
-    this.setState({
-      z: this.state.z * this.state.y
-    })
   }
 
-  divideZ = () => {
-    this.setState({
-      z: this.state.z / this.state.x  
-    })
+  export class MyProvider extends Component {
+
+    state = {
+      x: 0,
+      y: 100,
+      z: 50,
+      dispatch: (action) => {
+        this.setState((state) => reducer(state, action))
+      }
+    };
+      // we return a context provider
+
+    render() {
+
+      return (
+        // <MyContext.Provider value="I am the value">
+        <MyContext.Provider value={this.state}>
+          {this.props.children}
+        </MyContext.Provider>
+      )
+    }
   }
+  
 
-
-  render() {
-
-    console.log("./context.js <MyProvider /> this.state: ", this.state);
-
-    return (
-      // <MyContext.Provider value="I am the value">
-      <MyContext.Provider
-        value={{ 
-          state: this.state, 
-          incrementX: () => {this.setState({x: this.state.x + 1})},
-          decrementY: () => {this.setState({y: this.state.y - 1})},
-          multiplyZ: this.multiplyZ,
-          divideZ: this.divideZ
-        }}
-      >
-        {this.props.children}
-      </MyContext.Provider>
-    )
-  }
-}
